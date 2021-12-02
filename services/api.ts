de1873726d2af3b9582315e58b28ba739691e499
Thiago <tms1_tms2@hotmail.com>
2021-12-01 21:09:00 -0300

    Pass authorization token

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
+import { parseCookies } from "nookies";
+
+const cookies = parseCookies();
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: "http://localhost:3333",
+  headers: {
+    // Importante atualizar esse header depois de fazer login
+    Authorization: `Bearer ${cookies["nextauth.token"]}`
+  }
 });
